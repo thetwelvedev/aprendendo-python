768c6a3dcd469a641dd3084b368b62e4c8b8843c
Leonardo Castro <twelve.programming@gmail.com>
2024-12-08 16:43:17 -0400

    Estudo dos métodos estaticos e métodos de classe

--- python-orientado-a-objetos/pessoa.py
+++ python-orientado-a-objetos/pessoa.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import randint
 class Pessoa:
     ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
     def __init__(self, nome, idade, comendo=False, falando=False):
@@ -6,7 +7,7 @@ class Pessoa:
         self.idade = idade
         self.comendo = comendo
         self.falando = falando
-
+#métodos de instância - métodos com o parâmetro self que é usado para referenciar a própria instância do objeto dentro do método.
     def falar(self, assunto):
         if self.comendo:
              print(f"{self.nome} não pode falar comendo.")
@@ -49,3 +50,23 @@ class Pessoa:
 
     def get_ano_nascimento(self):
         return self.ano_atual - self.idade
+    
+#Método de classe (@classmethod) - que são métodos que pertencem à classe em si, não às suas instâncias.
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimneto):
+        idade = cls.ano_atual - ano_nascimneto
+        return cls(nome, idade)
+
+#Método estáticos  (@staticmethod) - que são métodos que não referenciam a classe ou instância em seu corpo. São similares à funções normais, porém, por questão de organização, são criados dentro da classe.
+    @staticmethod
+    def gerar_id():
+        rand = randint(10000, 19999)
+        return rand
+
+
+p1 = Pessoa.por_ano_nascimento('Luiz', 1992)
+#p1 = Pessoa('Luiz', 32)
+print(p1)
+print(p1.idade)
+print(p1.get_ano_nascimento())
+print(Pessoa.gerar_id())
