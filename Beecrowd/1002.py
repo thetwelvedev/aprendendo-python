bf6d1dbccc2370bece433d75025a8b1f5d6d7241
Leonardo Castro <twelve.programming@gmail.com>
2025-09-13 12:00:00 -0400

    Código 1002

---
+++ Beecrowd/1002.py
@@ -0,0 +1,7 @@
+"""A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π."""
+raio = float(input())
+n = 3.14159
+area = n*raio**2
+
+print(f"A={area:.4f}")
