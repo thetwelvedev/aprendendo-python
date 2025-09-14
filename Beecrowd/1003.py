6dc7c507819287e78ddf395c50d7d41079045f97
Leonardo Castro <twelve.programming@gmail.com>
2025-09-14 12:00:00 -0400

    Código 1003

---
+++ Beecrowd/1003.py
@@ -0,0 +1,6 @@
+"""Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável."""
+a = int(input())
+b = int(input())
+soma = a + b
+
+print(f"SOMA = {soma}")
