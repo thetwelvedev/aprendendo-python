bc52585042241a0253434202c75e99fc39a81014
Leonardo Castro <twelve.programming@gmail.com>
2025-09-15 12:00:00 -0400

    Código 1004

---
+++ Beecrowd/1004.py
@@ -0,0 +1,6 @@
+"""Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.  """
+a = int(input())
+b = int(input())
+produto = a*b
+
+print(f"PROD = {produto}")
