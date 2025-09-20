c2786cd3f7d87c5c2450fe847c9d8e33ed2ace0c
Leonardo Castro <twelve.programming@gmail.com>
2025-09-20 14:00:00 -0400

    Código 1006

---
+++ Beecrowd/1006.py
@@ -0,0 +1,8 @@
+"""Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal."""
+a = float(input())
+b = float(input())
+c = float(input())
+
+media = (a*2 + b*3 + c*5)/10
+
+print(f'MEDIA = {media:.1f}')
