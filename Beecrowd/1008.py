0e19d8f7d625922285373a986b95a3cc8e35fd4b
Leonardo Castro <twelve.programming@gmail.com>
2025-10-23 00:16:34 -0400

    Código 1008(correção de enunciado)

--- Beecrowd/1008.py
+++ Beecrowd/1008.py
@@ -1,5 +1,4 @@
-"""Timelimit: 1
-Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais."""
+"""Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais."""
 numero_funcionario = int(input())
 horas_trabalhadas = int(input())
 valor_por_hora_trabalhada = float(input())
