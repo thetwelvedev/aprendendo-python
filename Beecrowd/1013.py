568ec91bcbb738d68acb526bee77d929fd6c058c
Leonardo Castro <twelve.programming@gmail.com>
2025-10-23 00:20:43 -0400

    Código 1013(adição do enunciado)

--- Beecrowd/1013.py
+++ Beecrowd/1013.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
 a, b, c = map(float, input("").split())
 maiorAB = (a + b + abs(a - b)) // 2
 maior = (maiorAB + c + abs(maiorAB - c)) // 2
