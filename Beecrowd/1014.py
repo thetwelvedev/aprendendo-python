f39c60135f3a1fd59505535cbee3b3316b8b0333
Leonardo Castro <twelve.programming@gmail.com>
2025-10-23 00:21:33 -0400

    Código 1014(adição do enunciado)

--- Beecrowd/1014.py
+++ Beecrowd/1014.py
@@ -1,3 +1,4 @@
+#Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
 km = int(input(""))
 l = float(input(""))
 consumoMedio = km/l
